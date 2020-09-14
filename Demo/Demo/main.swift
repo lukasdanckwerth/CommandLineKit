eb7d641247c5e4a0cb8e29ed36227a730d5dc0ad
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-09-14 18:02:41 +0200

    add Demo

---
+++ Demo/Demo/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  CommandLineKit Demo
+//
+//  Created by Lukas Danckwerth on 14.09.20.
+//  Copyright © 2020 Anika. All rights reserved.
+//
+
+import Foundation
+import CommandLineKit
+
+print("Hello, World!")
+
